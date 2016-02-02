61d96defe5ec037b4a2980ec7adc87b30377cc11
jim <jim@vsiwest.com>
2016-02-02 01:06:02 -0800

    teraformed maven

---
+++ raccoon-raccoon/src/main/java/raccoon/raccoon/App.java
@@ -0,0 +1,13 @@
+package raccoon.raccoon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
